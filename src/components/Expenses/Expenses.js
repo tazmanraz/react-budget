574a3a78db11ab338e14086f71a8e22037027ba8
Imtiaz <imtiazmiah@gmail.com>
2021-07-20 02:25:15 -0400

    Add ExpenseList

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
+import ExpensesList from './ExpensesList';
 import './Expenses.css'
 
 
@@ -30,34 +31,16 @@ function Expenses(props) {
     <div>
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {filteredExpenses.length === 0 ? <p>No expenses found</p>:
+      {/* {filteredExpenses.length === 0 ? <p>No expenses found</p>:
       filteredExpenses.map(expense => <ExpenseItem 
         key={expense.id}
         title={expense.title} 
         amount={expense.amount}
         date={expense.date}
-      />)}
-{/* This is coded out to favour the above method which renders whole list dynamically */}
-      {/* <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      /> */}
+      />)} */}
+      <ExpensesList items={filteredExpenses}/>
+
+
     </Card>
     </div>
   )
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ExpenseItem from './ExpenseItem'
+import './ExpensesList.css'
+
+const ExpensesList = props => {
+  if (props.items.length === 0) {
+    return <h2 className='expenses-list__fallback'>Found no expense</h2>
+  }
+
+//   if (props.items.length > 0 ) {
+//     expensesContent = props.items.map(expense => <ExpenseItem 
+//       key={expense.id}
+//       title={expense.title} 
+//       amount={expense.amount}
+//       date={expense.date}
+//     />)}
+// };
+
+  return (
+  <li className="expenses-list">
+    {props.items.map(expense => <ExpenseItem 
+      key={expense.id}
+      title={expense.title} 
+      amount={expense.amount}
+      date={expense.date}
+    />)}
+  </li>
+  )
+    }
+export default ExpensesList
