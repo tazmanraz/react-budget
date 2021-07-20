c1ff4782a86bd7366bcce7e22948c5567a956620
Imtiaz <imtiazmiah@gmail.com>
2021-07-20 01:55:07 -0400

    Add conditional for expense item

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,11 +15,23 @@ function Expenses(props) {
 
   const filteredExpenses = props.items.filter(el => el.date.getFullYear().toString() === filteredYear)
 
+  // Alternatively, you can use this instead of ternary
+  // let expensesContent = <p>No expenses found</p>
+
+  // if (filteredExpenses.length > 0 ) {
+  //   filteredExpenses.map(expense => <ExpenseItem 
+  //     key={expense.id}
+  //     title={expense.title} 
+  //     amount={expense.amount}
+  //     date={expense.date}
+  //   />)}
+
   return (
     <div>
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {filteredExpenses.map(expense => <ExpenseItem 
+      {filteredExpenses.length === 0 ? <p>No expenses found</p>:
+      filteredExpenses.map(expense => <ExpenseItem 
         key={expense.id}
         title={expense.title} 
         amount={expense.amount}
