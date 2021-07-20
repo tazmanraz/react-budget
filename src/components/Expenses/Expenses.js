2aceb849abc80e8963fb93e3c3cdb5df5f854802
Imtiaz <imtiazmiah@gmail.com>
2021-07-20 01:44:58 -0400

    Add list filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,11 +13,14 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   }
 
+  const filteredExpenses = props.items.filter(el => el.date.getFullYear().toString() === filteredYear)
+
   return (
     <div>
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {props.items.map(expense => <ExpenseItem 
+      {filteredExpenses.map(expense => <ExpenseItem 
+        key={expense.id}
         title={expense.title} 
         amount={expense.amount}
         date={expense.date}
